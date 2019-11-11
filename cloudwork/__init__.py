18a77689d7f4e90d06bf94d58309e6e6d4cd3f44
Andreas Hortlund <hortlund.andreas@gmail.com>
2019-11-11 17:36:30 +0100

    Initial setup
    Created Virtual Enviroment
    Installed Flask
    Installed and updated dependencies

---
+++ cloudwork/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask
+
+# Code template from https://flask.palletsprojects.com/en/1.1.x/tutorial/factory/
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = os.path.join(app.instance_path, 'cloudwork.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Cloudworks!'
+
+    return app
+        
+
